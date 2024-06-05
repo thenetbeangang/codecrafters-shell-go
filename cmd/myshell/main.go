a48dbb2f591a00c4fbf6f277c8b7dae170902485
thenetbeangang <Caorey13@hotmail.com>
2024-06-05 11:00:44 +0200

    Added for loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,13 +10,15 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
 
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		cmd, _ := reader.ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+		fmt.Printf("%s: command not found\n", cmd)
+		os.Exit(1)
+	}
 	
-	// Read a line of input from standard input (keyboard)
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 	
-	// Print the input (excluding the newline character) followed by ": command not found"
-	fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")
 }
